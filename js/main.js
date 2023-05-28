36a9942af9fe10c00cdf1eaf49bff2ae0bc362a2
Ahmetqaz <107630795+Ahmetqaz@users.noreply.github.com>
2023-05-28 14:49:38 +0500

    res

--- js/main.js
+++ js/main.js
@@ -36,35 +36,35 @@ if (header)
 ///
 ///
 /// tabEvents
-const toggleBody = (isClosed) => {
-  if (isClosed) {
-    document.body.classList.add("active");
-    if (menu) closeMenu();
-  } else {
-    document.body.classList.remove("active");
-  }
-};
-document.querySelectorAll(`[data-event="tabEvent"]`).forEach((eventBtn) => {
-  const tab = document.querySelector(eventBtn.getAttribute("data-tab"));
-  if (tab) {
-    eventBtn.onclick = (e) => {
-      e.preventDefault();
-      tab.classList.toggle("active");
-      toggleBody(tab.classList.contains("active"));
-    };
-    tab.onclick = (e) => {
-      if (e.target === e.currentTarget) {
-        tab.classList.toggle("active");
-        toggleBody(tab.classList.contains("active"));
-      }
-    };
-  }
-});
-document.querySelectorAll(`[data-toggle]`).forEach((toggleBtn) => {
-  console.log("btn ->");
-  toggleBtn.onclick = () =>
-    toggleBtn.classList.toggle(toggleBtn.getAttribute("data-toggle"));
-});
+// const toggleBody = (isClosed) => {
+//   if (isClosed) {
+//     document.body.classList.add("active");
+//     if (menu) closeMenu();
+//   } else {
+//     document.body.classList.remove("active");
+//   }
+// };
+// document.querySelectorAll(`[data-event="tabEvent"]`).forEach((eventBtn) => {
+//   const tab = document.querySelector(eventBtn.getAttribute("data-tab"));
+//   if (tab) {
+//     eventBtn.onclick = (e) => {
+//       e.preventDefault();
+//       tab.classList.toggle("active");
+//       toggleBody(tab.classList.contains("active"));
+//     };
+//     tab.onclick = (e) => {
+//       if (e.target === e.currentTarget) {
+//         tab.classList.toggle("active");
+//         toggleBody(tab.classList.contains("active"));
+//       }
+//     };
+//   }
+// });
+// document.querySelectorAll(`[data-toggle]`).forEach((toggleBtn) => {
+//   console.log("btn ->");
+//   toggleBtn.onclick = () =>
+//     toggleBtn.classList.toggle(toggleBtn.getAttribute("data-toggle"));
+// });
 ///
 ///
 ///
