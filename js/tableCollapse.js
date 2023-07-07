06a03312460cb624a1bc1d45589b759ef23c7a89
Ahmet <ahmetcanlyev@gmail.com>
2023-07-07 22:00:32 +0500

    changes

--- js/tableCollapse.js
+++ js/tableCollapse.js
@@ -10,13 +10,34 @@ tableDevider.forEach((table) => {
     });
   });
 });
-const tableBox = document.querySelectorAll(".table__box");
-tableBox.forEach((table) => {
-  const tableDropdown = table.querySelector(".tableDropdown");
-  const dropdownBtn = table.querySelector(".tableDropdown__button");
-  const td = table.parentElement;
-  dropdownBtn.addEventListener("click", () => {
-    tableDropdown.classList.toggle("active");
-    td.classList.toggle("active");
-  });
+
+const tabBtn = document.querySelectorAll(".tabBtn");
+const tabEvent = document.querySelectorAll(".tabEvent");
+tabBtn.forEach((e) => {
+  onTabClick(tabBtn, tabEvent, e);
 });
+function onTabClick(tabBtns, tabItems, item) {
+  item.addEventListener("click", function (e) {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+    if (currentBtn.classList.contains("active")) {
+      console.log("now active");
+      const faq = currentBtn.parentElement.querySelector(".tabEvent");
+      if (faq) {
+        faq.classList.remove("active");
+        currentBtn.classList.remove("active");
+      }
+    } else if (!currentBtn.classList.contains("active")) {
+      tabBtns.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
